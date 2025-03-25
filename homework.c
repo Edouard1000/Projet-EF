8ee501af07c812de0160d1d5b86eb9de597763c7
Théodore Moulaert <theodore.moulert@student.uclouvain.be>
2025-03-25 15:06:57 +0100

    hello

---
+++ homework.c
@@ -0,0 +1,291 @@
+#include "fem.h"
+#include <math.h>
+#include <stdbool.h>
+
+
+// === Fonction pour l'interpolation de la taille du maillage (Mesh) ===
+
+double hermiteInterpolation(double d, double d_star, double h0, double h_star) {
+    if (d >= d_star) return h_star;
+    double t = d / d_star;
+    return h0 + (h_star - h0) * (3 * t * t - 2 * t * t * t);
+}
+
+double geoSize(double x, double y){
+    femGeo* theGeometry = geoGetGeometry();
+    double h = theGeometry->h;
+
+    // Trouver le sommet du rail et points intermédiaires
+    static double yTop, X1, Y1, X2, Y2;
+    yTop = 172;
+    X1 = 37.85;
+    Y1 = 157.7;
+    X2 = -X1;
+    Y2 = Y1;
+
+    // Calcul des distances
+    double dTop = fabs(yTop - y);
+    double d1 = sqrt((x - X1) * (x - X1) + (y - Y1) * (y - Y1));
+    double d2 = sqrt((x - X2) * (x - X2) + (y - Y2) * (y - Y2));
+
+    // Tailles de mailles plus fines
+    double hTop = h * 0.1;
+    double hUpper = hermiteInterpolation(dTop, 35, hTop, h);
+    double hPoint1 = hermiteInterpolation(d1, 25, hTop, h);
+    double hPoint2 = hermiteInterpolation(d2, 25, hTop, h);
+
+    return fmin(fmin(hUpper, hPoint1), hPoint2); // Retourne la plus petite taille
+}
+
+
+// === Assemblage des éléments (LinearElasticity) ===
+
+
+// Function to check if a matrix is symmetrical
+bool isSymmetrical(double **matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void femElasticityAssembleElements(femProblem *theProblem) {
+  // Récupération des structures de données
+    femFullSystem  *theSystem = theProblem->system;
+    femIntegration *theRule = theProblem->rule;
+    femDiscrete    *theSpace = theProblem->space;
+    femGeo         *theGeometry = theProblem->geometry;
+    femNodes       *theNodes = theGeometry->theNodes;
+    femMesh        *theMesh = theGeometry->theElements;
+    double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
+    int iElem,iInteg,i,j,map[4],mapX[4],mapY[4];
+
+    // Paramètres du problème
+    int nLocal = theMesh->nLocalNode;
+    double a   = theProblem->A;
+    double b   = theProblem->B;
+    double c   = theProblem->C;
+    double rho = theProblem->rho;
+    double g   = theProblem->g;
+    double **A = theSystem->A;
+    double *B  = theSystem->B;
+
+
+    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
+        for (j=0; j < nLocal; j++) {
+            map[j]  = theMesh->elem[iElem*nLocal+j];
+            mapX[j] = 2*map[j];
+            mapY[j] = 2*map[j] + 1;
+            x[j]    = theNodes->X[map[j]];
+            y[j]    = theNodes->Y[map[j]];
+        }
+
+        for (iInteg=0; iInteg < theRule->n; iInteg++) {
+            double xsi    = theRule->xsi[iInteg];
+            double eta    = theRule->eta[iInteg];
+            double weight = theRule->weight[iInteg];
+            femDiscretePhi2(theSpace,xsi,eta,phi);
+            femDiscreteDphi2(theSpace,xsi,eta,dphidxsi,dphideta);
+
+            double dxdxsi = 0.0;
+            double dxdeta = 0.0;
+            double dydxsi = 0.0;
+            double dydeta = 0.0;
+            for (i = 0; i < theSpace->n; i++) {
+                dxdxsi += x[i]*dphidxsi[i];
+                dxdeta += x[i]*dphideta[i];
+                dydxsi += y[i]*dphidxsi[i];
+                dydeta += y[i]*dphideta[i];
+            }
+            double jac = fabs(dxdxsi * dydeta - dxdeta * dydxsi);
+
+            for (i = 0; i < theSpace->n; i++) {
+                dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / jac;
+                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac;
+            }
+            for (i = 0; i < theSpace->n; i++) {
+                for(j = 0; j < theSpace->n; j++) {
+                    A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] +
+                                            dphidy[i] * c * dphidy[j]) * jac * weight;
+                    A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] +
+                                            dphidy[i] * c * dphidx[j]) * jac * weight;
+                    A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] +
+                                            dphidx[i] * c * dphidy[j]) * jac * weight;
+                    A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] +
+                                            dphidx[i] * c * dphidx[j]) * jac * weight;
+                }
+                B[mapY[i]] -= phi[i] * g * rho * jac * weight; // Ajout du terme source
+            }
+        }
+    }
+
+    // Check if the matrix is symmetrical
+    if (isSymmetrical(theProblem->system->A, theProblem->system->size)) {
+        printf("The matrix is symmetrical.\n");
+    } else {
+        printf("The matrix is not symmetrical.\n");
+    }
+         
+}
+
+
+// Variables globales pour stocker les copies de A et B (pour le calcul des résidus)
+double **A_copy = NULL;
+double *B_copy  = NULL;
+
+void femElasticityAssembleNeumann(femProblem *theProblem)
+{
+    femFullSystem  *theSystem   = theProblem->system;
+    femIntegration *theRule     = theProblem->ruleEdge;
+    femDiscrete    *theSpace    = theProblem->spaceEdge;
+    femGeo         *theGeometry = theProblem->geometry;
+    femNodes       *theNodes    = theGeometry->theNodes;
+    femMesh        *theEdges    = theGeometry->theEdges;
+
+    double x[2], y[2], phi[2];
+    int iBnd, iElem, iInteg, iEdge, i, j, map[2], mapU[2];
+
+    int nLocal = 2;
+    double *B  = theSystem->B;
+
+    for (iBnd = 0; iBnd < theProblem->nBoundaryConditions; iBnd++)
+    {
+        femBoundaryCondition *theCondition = theProblem->conditions[iBnd];
+        femBoundaryType type = theCondition->type;
+        femDomain *domain = theCondition->domain;
+        double value = theCondition->value;
+
+        if (type == DIRICHLET_X || type == DIRICHLET_Y) { continue; }   // Ignore les Dirichlet
+
+        int shift = (type == NEUMANN_X) ? 0 : 1;                        // Décalage pour X ou Y
+
+        for (iEdge = 0; iEdge < domain->nElem; iEdge++)
+        {
+            iElem = domain->elem[iEdge];
+            for (j = 0; j < nLocal; j++)
+            {
+                map[j] = theEdges->elem[iElem * nLocal + j];
+                mapU[j] = 2 * map[j] + shift;
+                x[j] = theNodes->X[map[j]];
+                y[j] = theNodes->Y[map[j]];
+            }
+
+            double dx = x[1] - x[0];
+            double dy = y[1] - y[0];
+            double length = sqrt(dx * dx + dy * dy);
+            double jac = length / 2;
+
+            for (iInteg = 0; iInteg < theRule->n; iInteg++)
+            {
+                double xsi    = theRule->xsi[iInteg];
+                double weight = theRule->weight[iInteg];
+                femDiscretePhi(theSpace, xsi, phi);
+                for (i = 0; i < theSpace->n; i++) {
+                    B[mapU[i]] += phi[i] * value * jac * weight;
+                }
+            }
+        }
+    }
+}
+
+
+double *femElasticitySolve(femProblem *theProblem) {
+    femFullSystem *theSystem = theProblem->system;
+    femFullSystemInit(theSystem);
+
+    femElasticityAssembleElements(theProblem);
+    femElasticityAssembleNeumann(theProblem);
+
+    int size = theSystem->size;
+
+    // Allocation et copie de A et B pour le calcul des résidus
+    if (A_copy == NULL) {
+        A_copy = (double **)malloc(sizeof(double *) * size);
+        for (int i = 0; i < size; i++) {
+            A_copy[i] = (double *)malloc(sizeof(double) * size);
+        }
+    }
+    if (B_copy == NULL) {
+        B_copy = (double *)malloc(sizeof(double) * size);
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            A_copy[i][j] = theSystem->A[i][j];
+        }
+        B_copy[i] = theSystem->B[i];
+    }
+
+    // Application des conditions de Dirichlet
+    int *theConstrainedNodes = theProblem->constrainedNodes;
+    for (int i = 0; i < size; i++) {
+        if (theConstrainedNodes[i] != -1) {
+            double value = theProblem->conditions[theConstrainedNodes[i]]->value;
+            femFullSystemConstrain(theSystem, i, value);
+        }
+    }
+
+    // Résolution
+    femFullSystemEliminate(theSystem);
+    memcpy(theProblem->soluce, theSystem->B, theSystem->size * sizeof(double));
+    return theProblem->soluce;
+}
+
+
+double *femElasticityForces(femProblem *theProblem) {
+    double *residuals = theProblem->residuals;
+    double *soluce = theProblem->soluce;
+    int size = theProblem->system->size;
+
+    if (residuals == NULL) {
+        residuals = (double *)malloc(sizeof(double) * size);
+    }
+    for (int i = 0; i < size; i++) {
+        residuals[i] = 0.0;
+    }
+
+    // Calcul des résidus : R = A * U - B
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            residuals[i] += A_copy[i][j] * soluce[j];
+        }
+        residuals[i] -= B_copy[i];
+    }
+
+    // Libération de la mémoire des copies (IMPORTANT : évite les fuites de mémoire)
+    for (int i = 0; i < size; i++) {
+      free(A_copy[i]);
+      A_copy[i] = NULL;
+    }
+
+    free(A_copy);
+    free(B_copy);
+    A_copy = NULL;
+    B_copy = NULL;
+
+    return residuals;
+}
+
+
+
+void geoMeshGenerate() {
+    int ierr;
+
+    femGeo* theGeometry = geoGetGeometry();
+    
+    gmshOpen("/mnt/c/Users/theom/OneDrive - UCL/BAC3/Q6/Elements Finis/Projet/RailMesh/UIC60_copie2.geo", &ierr); //OUVRE LE FICHIER .GEO
+    ErrorGmsh(ierr);
+
+    geoSetSizeCallback(geoSize);
+
+    gmshModelOccSynchronize(&ierr); //SYNCHRONISE LE MODÈLE
+
+    // Option pour que Gmsh sauvegarde *tous* les éléments (pas seulement ceux avec des propriétés physiques)
+    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+
+     // Générer le maillage 2D
+    gmshModelMeshGenerate(2, &ierr);
+}
---
+++ main.c
@@ -0,0 +1,246 @@
+#include "glfem.h"
+
+
+// Fonction d'intégration (pour le calcul du poids)
+double fun(double x, double y) {
+    return 1;
+}
+
+int main(void) {
+    printf("\n\n    V : Mesh and displacement norm \n");
+    printf("    D : Domains \n");
+    printf("    X : Horizontal residuals for unconstrained equations \n");
+    printf("    Y : Horizontal residuals for unconstrained equations \n");
+    printf("    N : Next domain highlighted\n\n\n");
+
+    // Initialisation de la géométrie et de Gmsh
+    geoInitialize();
+    femGeo* theGeometry = geoGetGeometry();
+
+    theGeometry->h = 3;
+    theGeometry->elementType = FEM_TRIANGLE;
+    
+    geoMeshGenerate(); // Génère le maillage avec Gmsh
+    geoMeshImport();  // Importe le maillage dans les structures de données de fem.c
+    
+    // Nommer les domaines
+    // Domain Bottom
+    geoSetDomainName(22, "Bottom0");
+    geoSetDomainName(4, "Bottom1");
+    geoSetDomainName(5, "Bottom2");
+    geoSetDomainName(6, "Bottom3");
+    geoSetDomainName(0, "Bottom4");
+    geoSetDomainName(1, "Bottom5");
+    geoSetDomainName(2, "Bottom6");
+    geoSetDomainName(3, "Bottom7");
+    geoSetDomainName(17, "Bottom8");
+
+    // Domain Top
+    geoSetDomainName(15, "Patine0");
+    geoSetDomainName(16, "Patine1");
+
+    // Domain Gauche
+    geoSetDomainName(8, "Gauche0");
+    geoSetDomainName(10, "Gauche1");
+    geoSetDomainName(14, "Gauche2");
+
+
+    // Domain Droite
+    geoSetDomainName(7, "Droite0");
+    geoSetDomainName(9, "Droite1");
+    geoSetDomainName(11, "Droite2");
+
+    // Domain Haut
+    geoSetDomainName(12, "Haut0");
+    geoSetDomainName(21, "Haut1");
+    geoSetDomainName(20, "Haut2");
+    geoSetDomainName(19, "Haut3");
+    geoSetDomainName(18, "Haut4");
+    geoSetDomainName(13, "Haut5");
+
+
+
+
+    geoMeshWrite("../data/elasticity.txt"); // Optionnel: Sauvegarde du maillage
+
+
+    // Paramètres du problème d'élasticité
+    double E   = 211e9;     // Module de Young
+    double nu  = 0.3;      // Coefficient de Poisson
+    double rho = 7850;      // Masse volumique
+    double g   = 9.81;     // Accélération gravitationnelle
+
+
+    // Création du problème
+    femProblem* theProblem = femElasticityCreate(theGeometry, E, nu, rho, g, PLANAR_STRAIN);
+
+    // Ajout des conditions aux limites (Dirichlet et Neumann)
+
+    // Add boundary conditions for Bottom domains
+    femElasticityAddBoundaryCondition(theProblem, "Bottom0", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom1", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom2", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom3", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom4", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom5", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom6", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom7", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Bottom8", DIRICHLET_Y, 0.0);
+
+    // Add boundary conditions for Gauche domains
+    femElasticityAddBoundaryCondition(theProblem, "Gauche0", DIRICHLET_X, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Gauche1", DIRICHLET_X, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Gauche2", DIRICHLET_X, 0.0);
+
+    // Add boundary conditions for Droite domains
+    femElasticityAddBoundaryCondition(theProblem, "Droite0", DIRICHLET_X, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Droite1", DIRICHLET_X, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Droite2", DIRICHLET_X, 0.0);
+
+    // Add boundary conditions for Haut domains
+    femElasticityAddBoundaryCondition(theProblem, "Haut0", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Haut1", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Haut2", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Haut3", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Haut4", DIRICHLET_Y, 0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Haut5", DIRICHLET_Y, 0.0);
+
+
+
+    femElasticityAddBoundaryCondition(theProblem, "Patine0", NEUMANN_Y, -1e4);
+    femElasticityAddBoundaryCondition(theProblem, "Patine1", NEUMANN_Y, -1e4);
+    
+
+
+    femElasticityPrint(theProblem); // Affiche les paramètres du problème
+
+
+    // Résolution du problème
+    double *theSoluce = femElasticitySolve(theProblem);
+
+    // Calcul des forces résiduelles
+    double *theForces = femElasticityForces(theProblem);
+
+     // Intégration de la fonction 'fun' (pour le calcul du poids total)
+    double area = femElasticityIntegrate(theProblem, fun);
+
+    // Facteur d'amplification pour la visualisation des déplacements
+    femNodes *theNodes = theGeometry->theNodes;
+    double deformationFactor = 1e4; // A ajuster selon votre géométrie/déplacements
+
+    // Allocation de mémoire pour les champs à afficher
+    double *normDisplacement = malloc(theNodes->nNodes * sizeof(double));
+    double *forcesX = malloc(theNodes->nNodes * sizeof(double));
+    double *forcesY = malloc(theNodes->nNodes * sizeof(double));
+    
+    
+    for (int i = 0; i < theNodes->nNodes; i++) {
+        // Applique la déformation pour la visualisation
+        theNodes->X[i] += theSoluce[2 * i + 0] * deformationFactor;
+        theNodes->Y[i] += theSoluce[2 * i + 1] * deformationFactor;
+        // Calcule la norme du déplacement
+        normDisplacement[i] = sqrt(theSoluce[2 * i + 0] * theSoluce[2 * i + 0] +
+                                   theSoluce[2 * i + 1] * theSoluce[2 * i + 1]);
+        // Stocke les forces résiduelles
+        forcesX[i] = theForces[2 * i + 0];
+        forcesY[i] = theForces[2 * i + 1];
+    }
+
+    
+    // Affichage des déplacements min et max
+    double hMin = femMin(normDisplacement, theNodes->nNodes);
+    double hMax = femMax(normDisplacement, theNodes->nNodes);
+    printf(" ==== Minimum displacement          : %14.7e [m] \n", hMin);
+    printf(" ==== Maximum displacement          : %14.7e [m] \n", hMax);
+
+    // Calcul et affichage des forces globales
+    double theGlobalForce[2] = {0, 0};
+    for (int i = 0; i < theNodes->nNodes; i++) {
+        theGlobalForce[0] += theForces[2 * i + 0];
+        theGlobalForce[1] += theForces[2 * i + 1];
+    }
+    printf(" ==== Global horizontal force       : %14.7e [N] \n", theGlobalForce[0]);
+    printf(" ==== Global vertical force         : %14.7e [N] \n", theGlobalForce[1]);
+    printf(" ==== Weight                        : %14.7e [N] \n", area * rho * g);
+
+     // Initialisation de la fenêtre GLFW
+    GLFWwindow* window = glfemInit("EPL1110 : Linear Elasticity");
+    glfwMakeContextCurrent(window);
+
+    int mode = 1; // Mode d'affichage
+    int domain = 0; // Domaine courant
+    int freezingButton = FALSE;
+    double t, told = 0;
+    char theMessage[256];
+
+    // Boucle principale de l'application (boucle d'événements)
+    do {
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h); // Récupère la taille de la fenêtre
+        glfemReshapeWindows(theGeometry->theNodes, w, h); // Adapte la vue
+
+        t = glfwGetTime(); // Temps courant pour les animations
+
+        // Gestion des touches du clavier pour changer le mode d'affichage
+        if (glfwGetKey(window, 'D') == GLFW_PRESS) { mode = 0; }
+        if (glfwGetKey(window, 'V') == GLFW_PRESS) { mode = 1; }
+        if (glfwGetKey(window, 'X') == GLFW_PRESS) { mode = 2; }
+        if (glfwGetKey(window, 'Y') == GLFW_PRESS) { mode = 3; }
+        if (glfwGetKey(window, 'N') == GLFW_PRESS && freezingButton == FALSE) {
+            domain++;
+            freezingButton = TRUE;
+            told = t;
+        }
+        if (t - told > 0.5) {
+            freezingButton = FALSE;
+        }
+
+         // Affichage en fonction du mode
+        if (mode == 0) {
+            domain = domain % theGeometry->nDomains;  // Cycle sur les domaines
+            glfemPlotDomain(theGeometry->theDomains[domain]);  // Affiche le domaine courant
+            sprintf(theMessage, "%s : %d ", theGeometry->theDomains[domain]->name, domain);
+            glColor3f(1.0, 0.0, 0.0);
+            glfemMessage(theMessage); // Affiche le nom du domaine
+        }
+        if (mode == 1) {
+             // Affiche le champ de déplacement et le maillage déformé
+            glfemPlotField(theGeometry->theElements, normDisplacement);
+            glfemPlotMesh(theGeometry->theElements);
+            sprintf(theMessage, "Number of elements : %d ", theGeometry->theElements->nElem);
+            glColor3f(1.0, 0.0, 0.0);
+            glfemMessage(theMessage);  // Affiche le nombre d'éléments
+        }
+        if (mode == 2) {
+             // Affiche les forces résiduelles horizontales et le maillage
+            glfemPlotField(theGeometry->theElements, forcesX);
+            glfemPlotMesh(theGeometry->theElements);
+            sprintf(theMessage, "Number of elements : %d ", theGeometry->theElements->nElem);
+            glColor3f(1.0, 0.0, 0.0);
+            glfemMessage(theMessage); // Affiche le nombre d'éléments
+        }
+        if (mode == 3) {
+            // Affiche les forces résiduelles verticales et le maillage
+            glfemPlotField(theGeometry->theElements, forcesY);
+            glfemPlotMesh(theGeometry->theElements);
+            sprintf(theMessage, "Number of elements : %d ", theGeometry->theElements->nElem);
+            glColor3f(1.0, 0.0, 0.0);
+            glfemMessage(theMessage);  // Affiche le nombre d'éléments
+        }
+        glfwSwapBuffers(window); // Swap des buffers (double buffering)
+        glfwPollEvents();       // Traite les événements (clavier, souris, etc.)
+
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             glfwWindowShouldClose(window) != 1);
+
+    // Libération de la mémoire (très important !)
+    free(normDisplacement);
+    free(forcesX);
+    free(forcesY);
+    femElasticityFree(theProblem); // Libère *toutes* les structures du problème
+    geoFinalize();      // Finalise Gmsh et libère la géométrie
+    glfwTerminate();    // Termine GLFW
+
+    exit(EXIT_SUCCESS); // Fin du programme
+    return 0;
+}
